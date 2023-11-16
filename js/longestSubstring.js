f50f865a74fe8a45f968d14b88f2b6dbc5f1f603
Manasseh <believemanasseh@gmail.com>
2023-11-16 18:22:15 +0100

    feat: add new js solution

---
+++ js/longestSubstring.js
@@ -0,0 +1,25 @@
+function longestSubstring(string) {
+  let count = 0;
+  let temp = [];
+
+  for (let i = 0; i < string.length; i++) {
+    if (!temp.includes(string[i])) {
+      temp.push(string[i]);
+    } else {
+      if (temp.length > count) {
+        count = temp.length;
+      }
+      temp.length = 0;
+      temp.push(string[i]);
+    }
+    if (i == string.length - 1 && count == 0) {
+      return temp.length;
+    }
+  }
+  return count;
+}
+
+const strings = ["abcabccb", "dorime", "hokpol"];
+for (string of strings) {
+  console.log(longestSubstring(string));
+}
