66fad77d5f554208e25194e195a520fa645da567
Manasseh <believemanasseh@gmail.com>
2023-11-16 18:04:45 +0100

    feat: add new kotlin solution

---
+++ kt/longestSubstring.kt
@@ -0,0 +1,27 @@
+fun main() {
+  val strings = listOf("abcabccb", "dorime", "hopjol")
+  for (string in strings) {
+    println(solution(string))
+  }
+}
+
+fun solution(string: String): Int {
+  var count = 0
+  val temp: MutableList<Char> = mutableListOf()
+
+  for ((index, ch) in string.withIndex()) {
+    if (ch !in temp) {
+      temp.add(ch)
+    } else {
+      if (temp.size > count) {
+        count = temp.size
+      }
+      temp.clear()
+      temp.add(ch)
+    }
+    if (index == string.length - 1 && count == 0) {
+      return temp.size
+    }
+  }
+  return count
+}
