d79b58ae2e6b0902c52cc1c05f446a770f410892
Manasseh <believemanasseh@gmail.com>
2023-11-25 02:39:35 +0100

    feat: add new c++ solution

---
+++ c++/counting_elements.cpp
@@ -0,0 +1,38 @@
+/*C++17
+
+Time complexity: O(n)
+Space complexity: O(m)
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+int solution(vector<int> nums)
+{
+    int res = 0;
+    unordered_map<int, int> counter;
+
+    for (const auto &num : nums)
+    {
+        counter[num]++;
+    }
+
+    for (int i = 0; i < nums.size() - 1; i++)
+    {
+        if (counter[nums[i] + 1] > 0)
+        {
+            res++;
+        }
+    }
+    return res;
+}
+
+int main()
+{
+    cout << solution({1, 2, 3}) << endl;
+    return 0;
+}
